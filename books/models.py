e2b354457b3e7073868ae156dbfd8afb618296a6
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-29 11:45:39 +0300

    Create Borrowing model with data validation & representation. Update Book model.

--- books/models.py
+++ books/models.py
@@ -13,5 +13,8 @@ class Book(models.Model):
     inventory = models.PositiveSmallIntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
 
+    class Meta:
+        ordering = ["author"]
+
     def __str__(self) -> str:
         return f"{self.title} ({self.author})"
--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(blank=False, null=False)
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(
+        to=Book,
+        on_delete=models.PROTECT,
+        related_name="borrowings",
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name="borrowings",
+    )
+
+    class Meta:
+        ordering = ["expected_return_date"]
+
+    def clean(self) -> None:
+        if self.expected_return_date <= self.borrow_date:
+            raise ValidationError(_(
+                "Expected return date has to be after the borrowing date."
+            ))
+
+        if self.actual_return_date and (
+            self.actual_return_date < self.borrow_date
+        ):
+            raise ValidationError(_(
+                "Actual returning date cannot be earlier "
+                "than the borrowing date."
+            ))
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return (
+            f"Book: {self.book.title} ({self.book.author}) "
+            f"borrowed by {self.user.first_name} {self.user.last_name}."
+            f"Expected return date: {self.expected_return_date}"
+        )
