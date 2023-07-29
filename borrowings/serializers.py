d9046a6ed7911262389c60ba91c1db5412e542da
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-29 13:27:39 +0300

    Add CreateBorrowingSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from borrowings.models import Borrowing
@@ -30,3 +31,31 @@ class BorrowingDetailSerializer(BorrowingListSerializer):
             "book",
             "user"
         )
+
+
+class CreateBorrowingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "book",
+            "expected_return_date",
+        )
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        return super().create(validated_data)
+
+    def validate(self, data):
+        book = data.get("book")
+
+        if book.inventory == 0:
+            raise serializers.ValidationError(
+                "This book is not available for borrowing now"
+            )
+
+        return data
