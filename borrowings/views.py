0fd8e05f4522c2c707938e8dd18a704087570266
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-29 19:08:02 +0300

    Implement return_book function for borrowings return

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,7 +1,14 @@
-from rest_framework import mixins, viewsets
-from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+
+from django.db import transaction
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import (
     BorrowingListSerializer,
@@ -50,3 +57,34 @@ class BorrowingViewSet(
             return BorrowingDetailSerializer
 
         return CreateBorrowingSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="return",
+        permission_classes=[IsAdminUser],
+    )
+    def return_book(self, request, pk=None):
+        """Endpoint for borrowing returning"""
+
+        with transaction.atomic():
+            borrowing = self.get_object()
+
+            if not borrowing.actual_return_date:
+                serializer = self.get_serializer(borrowing, data=request.data)
+                serializer.is_valid(raise_exception=True)
+                borrowing.actual_return_date = serializer.validated_data.get(
+                    "actual_return_date"
+                )
+                borrowing.save()
+
+                book_id = borrowing.book_id
+                book = get_object_or_404(Book, id=book_id)
+                book.inventory += 1
+                book.save()
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            return Response(
+                {"details": "It is impossible to return borrowed book twice"}
+            )
