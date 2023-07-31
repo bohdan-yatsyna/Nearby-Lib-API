06c2377deeca1d9faf3389192e2f6984d49712d5
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-31 17:10:48 +0300

    Add str method for payment model

--- payments/models.py
+++ payments/models.py
@@ -23,3 +23,9 @@ class Payment(models.Model):
     session_url = models.CharField(max_length=1000, null=True, blank=True)
     session_id = models.CharField(max_length=1000, null=True, blank=True)
     to_pay = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return (
+            f"Borrowing ID: {self.borrowing_id}, "
+            f"payment status: {self.status}"
+        )
