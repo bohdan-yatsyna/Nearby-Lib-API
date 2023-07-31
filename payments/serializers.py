11b7ef1c8f16751e58414795761c81ada9e89185
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-31 17:22:52 +0300

    Add PaymentDetailSerializer in payments app

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
+from borrowings.serializers import BorrowingDetailSerializer
 from payments.models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing_id",
+            "session_url",
+            "session_id",
+            "to_pay",
+        )
+
+
+class PaymentDetailSerializer(PaymentSerializer):
+    borrowing_id = BorrowingDetailSerializer(many=False, read_only=True)
 
     class Meta:
         model = Payment
