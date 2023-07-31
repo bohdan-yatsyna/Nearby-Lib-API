dc3941df5caad42d435f1ee404ca02f24b8a89f5
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-31 15:51:08 +0300

    Add celery configurations to settings.py

--- library_app/settings.py
+++ library_app/settings.py
@@ -148,3 +148,9 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Kyiv"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
