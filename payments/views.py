5cb83720ed62a73c1d134ecf0bdb358286086c63
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-07-31 19:41:10 +0300

    Create PaymentViewSet

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Type
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import Serializer
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from payments.models import Payment
+from payments.serializers import PaymentSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self) -> Type[Serializer]:
+
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentSerializer
