d1521792e40dbcfa1022fc6587653e8543ae4e22
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-01 14:59:50 +0300

    Little change of send_message function, notifications app

--- notifications/notifications_bot.py
+++ notifications/notifications_bot.py
@@ -9,12 +9,12 @@ from users.models import User
 
 load_dotenv()
 
-token = os.getenv("TELEGRAM_BOT_TOKEN")
-bot = telebot.TeleBot(token=token)
-chat_id = os.getenv("TELEGRAM_CHAT_ID")
-
 
 def send_message(message: str) -> Any:
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    bot = telebot.TeleBot(token=token)
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
     return bot.send_message(chat_id, message)
 
 
