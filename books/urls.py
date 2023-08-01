e6f37b80c657eec3477368c75ea2e29b53cae27e
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-01 09:29:42 +0300

    Update endpoint for Books application according project requirements

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,6 @@ from books.views import BookViewSet
 app_name = "books"
 
 router = routers.DefaultRouter()
-router.register("books", BookViewSet, basename="book")
+router.register("", BookViewSet, basename="book")
 
 urlpatterns = [] + router.urls
