52887e609afda8de643e8f16ba28b256eaacf877
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-01 11:47:54 +0300

    Fix get_queryset method in PaymentViewSet

--- payments/views.py
+++ payments/views.py
@@ -31,16 +31,10 @@ class PaymentViewSet(
         return PaymentSerializer
 
     def get_queryset(self) -> QuerySet[Payment]:
+        user = self.request.user
         queryset = self.queryset
 
-        if self.request.user.is_staff:
-            user_id = self.request.query_params.get("user_id")
-
-            if user_id:
-                queryset = queryset.filter(user_id=user_id)
-
-            return queryset
-
-        queryset = queryset.filter(borrowing_id__user=self.request.user.id)
+        if user.is_staff is False:
+            queryset.filter(user=user)
 
         return queryset
