6a8e3ee444d342b0826def875d92b8819106f8dc
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-01 17:41:48 +0300

    Add documentation details for BookViewSet endpoints

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -7,6 +8,27 @@ from books.serializers import BookSerializer
 from library_app.pagination import Pagination
 
 
+# Only for documentation purposes (Swagger)
+@extend_schema_view(
+    list=extend_schema(
+        description="Endpoint showing list of all books in the system"
+    ),
+    retrieve=extend_schema(
+        description="Endpoint showing specific book by it id"
+    ),
+    create=extend_schema(
+        description="Endpoint for creating book, admin users only"
+    ),
+    update=extend_schema(
+        description="Endpoint for updating book, admin users only"
+    ),
+    partial_update=extend_schema(
+        description="Endpoint for partial book updating, admin users only"
+    ),
+    destroy=extend_schema(
+        description="Endpoint for deleting book, admin users only"
+    ),
+)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
