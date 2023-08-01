e0388941b9c85262dd7eede7c6a5f4ce237fc450
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-01 17:53:47 +0300

    Add documentation details for User Views endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -5,11 +6,29 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from users.serializers import UserSerializer
 
 
+# Only for documentation purposes (Swagger)
+@extend_schema_view(
+    post=extend_schema(
+        description="Endpoint for creating user, no permissions required"
+    )
+)
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
 
+# Only for documentation purposes (Swagger)
+@extend_schema_view(
+    get=extend_schema(
+        description="Endpoint with detail information about current user"
+    ),
+    put=extend_schema(
+        description="Endpoint for update current user details"
+    ),
+    patch=extend_schema(
+        description="Endpoint for partial update current user details"
+    ),
+)
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (JWTAuthentication,)
