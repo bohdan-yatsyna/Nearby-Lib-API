90af298a45ea96b87bcbe82d1b76a3342de4677b
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-02 14:31:15 +0300

    Add test for Payment model

---
+++ payments/tests/test_payment_model.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from books.models import Book
+from borrowings.models import Borrowing
+from payments.models import Payment
+from decimal import Decimal
+
+
+class PaymentModelTest(TestCase):
+    def setUp(self) -> None:
+        self.book = Book.objects.create(
+            title="Shantaram",
+            author="Gregory David Roberts",
+            cover=Book.CoverChoices.SOFT,
+            inventory=7,
+            daily_fee=0.12,
+        )
+
+        self.user = get_user_model().objects.create_user(
+            "test@test.com", "TestPassword1"
+        )
+
+        self.borrowing = Borrowing.objects.create(
+            expected_return_date="2023-10-25",
+            book=self.book,
+            user=self.user,
+        )
+
+    def test_payment_str_representation(self):
+        payment = Payment.objects.create(
+            status=Payment.StatusChoices.PENDING,
+            type=Payment.TypeChoices.PAYMENT,
+            borrowing_id=self.borrowing,
+            to_pay=Decimal("12.55"),
+        )
+        expected_str_representation = (
+            f"Borrowing ID: {self.borrowing}, "
+            f"payment status: {Payment.StatusChoices.PENDING}"
+        )
+
+        self.assertEqual(str(payment), expected_str_representation)
